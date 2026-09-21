42a607fefd0b9dd66291baf44b6f69d6173908d6
派蒙！ <paimoncai.0@gmail.com>
2026-09-22 00:22:38 +0800

    feat: export ProjectState and schema API

--- research/dsh_research/__init__.py
+++ research/dsh_research/__init__.py
@@ -9,22 +9,30 @@ from .config import load_research_config, load_yaml, write_yaml
 from .hashing import FileFingerprint, fingerprint_file, sha256_file, tree_digest
 from .manifests import load_json, read_jsonl, write_json, write_jsonl
 from .project import ResearchProject, find_project_root
+from .schema import CURRENT_PROJECT_SCHEMA, migrate_config, migrate_project, validate_project_schema
+from .state import ProjectState, build_project_state
 from .vcs import GitState, git_state
 
 __all__ = [
     "FileFingerprint",
     "GitState",
     "ResearchProject",
+    "ProjectState",
+    "CURRENT_PROJECT_SCHEMA",
+    "build_project_state",
     "find_project_root",
     "fingerprint_file",
     "git_state",
     "load_json",
     "load_research_config",
     "load_yaml",
+    "migrate_config",
+    "migrate_project",
     "read_jsonl",
     "sha256_file",
     "tree_digest",
     "write_json",
     "write_jsonl",
+    "validate_project_schema",
     "write_yaml",
 ]
